78a72c720c4766428d527d48aad635455b62b0a5
Surpent <surpenter@gmail.com>
2018-12-08 22:57:16 +0900

    変数の大文字・小文字は別物扱いになる

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 "use strict"; // レガシーな書き方とかを例外扱いにしてくれるやつ。最初に書く
 
 // 変数宣言時は const(定数) let(定数以外) を付けないと例外扱いになる
+// 変数の大文字・小文字は別物扱いになる
 const TEST = "Hello Work";
+const test = "Hello World";
 let Unko = "toilet";
 console.log(TEST);
+console.log(test);
 console.log(Unko);
 Unko = "Toilet Ikitai";
 console.log(Unko);
