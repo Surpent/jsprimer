fff0adc0d0475f6dc03edff991363d55efbfa822
Surpent <surpenter@gmail.com>
2018-12-08 22:55:56 +0900

    変数 const let の使い方

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 "use strict"; // レガシーな書き方とかを例外扱いにしてくれるやつ。最初に書く
+
+// 変数宣言時は const(定数) let(定数以外) を付けないと例外扱いになる
+const TEST = "Hello Work";
+let Unko = "toilet";
+console.log(TEST);
+console.log(Unko);
+Unko = "Toilet Ikitai";
+console.log(Unko);
