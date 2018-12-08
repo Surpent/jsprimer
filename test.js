40acf5da2ed74d021cff7fee9cdff29811950ddb
Surpent <surpenter@gmail.com>
2018-12-08 23:12:07 +0900

    変数の書き方について

--- test.js
+++ test.js
@@ -21,3 +21,24 @@ console.log("てすとだよん");
 console.log(Unko);
 Unko = "Toilet Ikitai";
 console.log(Unko);
+
+
+// 変数の宣言について
+// , (カンマ) で区切ることができる
+
+var iPhone = "iPhone",
+    Android = "Xperia";
+
+console.log(iPhone);
+console.log(Android);
+
+// 変数名に var などの予約語は使えない
+// 2nd など先頭に数字があるのもNG
+// 使える変数を確認するには以下のサイトが有効
+// https://mothereff.in/js-variables
+
+var $; // OK: $から開始できる
+var _title; // OK: _から開始できる
+var jquery; // OK: アルファベット
+var es2015; // OK: 数字は先頭以外なら利用できる
+var valid日本語; // OK: 先頭以外なら一部Unicode文字も利用可能
